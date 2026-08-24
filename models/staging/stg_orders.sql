f5d9b2782cac438ab68e98e65162a821f681ebce
samaraon94@gmail.com <samaraon94@gmail.com>
2026-08-24 11:23:40 +0000

    Updated

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,16 +1,21 @@
 select 
-    o.orderid,
-    o.orderdate,
-    o.shipdate,
-    o.shipmode,
-    o.ordersellingprice - o.ordercostprice as orderprofit,
-    o.ordersellingprice,
-    o.ordercostprice,
- 
-    p.productname,
-    p.subcategory,
-    p.category
+    o.ORDERID,
+    o.ORDERDATE,
+    o.SHIPDATE,
+    o.SHIPMODE,
+    o.CUSTOMERID,
+    o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE as orderprofit,
+    o.ORDERSELLINGPRICE,
+    o.ORDERCOSTPRICE,
+    c.CustomerName,
+    c.Segment,
+    c.Country,
+    p.ProductID,
+    p.ProductName,
+    p.SubCategory,
+    p.Category
     from {{ ref('orders') }} as o
-    
+    left join {{ ref('customers') }} as c
+    on o.CUSTOMERID = c.CUSTOMERID
     left join {{ ref('products') }} as p
-    on o.productid = p.productid
+    on o.ProductID = p.ProductID
